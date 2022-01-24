b3360ba50603c9937f6636c0cf4b8281a2377dfc
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-01-24 16:00:05 -0600

    Create initial header, install fontawesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
-    return (
-        <div />
-    );
+  return (
+    <header>
+      <nav className="w-screen bg-red-500 flex py-4 justify-between items-center">
+        <h1 className="pl-5 text-3xl">ShopTrack</h1>
+        <a href="https://github.com/dylan-brackett/shopping-planner">
+          <div className="text-5xl pr-5">
+            <FontAwesomeIcon icon={faGithub} />
+          </div>
+        </a>
+      </nav>
+    </header>
+  );
 }
