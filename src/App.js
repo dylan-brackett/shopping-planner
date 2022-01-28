e915a23999f4b8c28176ea7a808ea55770617a7b
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-01-28 16:59:27 -0600

    Add input form to add items to the list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
+import Form from "./components/Form";
 
 function App() {
   const [listItems, setListItems] = useState([
@@ -18,8 +19,8 @@ function App() {
   return (
     <div className="app bg-gradient-to-b from-green-500 to-green-300 h-screen">
       <Header />
-      <div className="max-w-lg mx-auto bg-white my-8 rounded-lg p-10 shadow-xl">
-        <h1 className="text-center">Shopping Tracker</h1>
+      <div className="max-w-xl mx-auto bg-white my-8 rounded-lg p-10 shadow-xl">
+        <Form addItem={addItem} />
         <List listItems={listItems} />
       </div>
     </div>
