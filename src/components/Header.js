d028b89ffe60c76145576f69aaa942a1130d8fef
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-01-31 16:55:42 -0600

    Open GitHub link in new tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,11 @@ export default function Header() {
     <header>
       <nav className="w-screen bg-zinc-50 shadow shadow-gray-600 flex py-4 justify-between items-center">
         <h1 className="pl-5 text-3xl">ShopPlan</h1>
-        <a href="https://github.com/dylan-brackett/shopping-planner">
+        <a
+          href="https://github.com/dylan-brackett/shopping-planner"
+          target="_blank"
+          rel="noreferrer"
+        >
           <div className="text-5xl pr-5">
             <FontAwesomeIcon icon={faGithub} />
           </div>
