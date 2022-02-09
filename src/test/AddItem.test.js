62c13b3265c03a8c11cdd66d2b551c738a2b6fab
Dylan Brackett <dylan.t.brackett@gmail.com>
2022-02-08 19:45:43 -0600

    Remove unneeded imports

--- src/test/AddItem.test.js
+++ src/test/AddItem.test.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  render,
-  screen,
-  fireEvent,
-  queryByTestId,
-  getByTestId,
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ShoppingPlanner from "../components/ShoppingPlanner";
 
